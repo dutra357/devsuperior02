e73b56b772d05203a47f2ce8640b5a63ab7fc995
David Dutra <144396143+dutra357@users.noreply.github.com>
2024-11-18 15:23:03 -0300

    Spring initializer. Starter.
    Projeto base do Spring initializer.

---
+++ src/main/java/com/dominio/devsuperior02/Devsuperior02Application.java
@@ -0,0 +1,13 @@
+package com.dominio.devsuperior02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devsuperior02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devsuperior02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominio/devsuperior02/Devsuperior02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominio.devsuperior02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devsuperior02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
